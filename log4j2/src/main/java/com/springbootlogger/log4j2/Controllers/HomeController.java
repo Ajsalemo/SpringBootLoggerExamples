1fef68a1d8aa55e4d184b739d7779ed52c36257d
Anthony Salemo <ajssalemo@gmail.com>
2021-06-11 13:29:31 -0400

    added a log4j2.yml config file

--- log4j2/src/main/java/com/springbootlogger/log4j2/Controllers/HomeController.java
+++ log4j2/src/main/java/com/springbootlogger/log4j2/Controllers/HomeController.java
@@ -1,14 +1,19 @@
 package com.springbootlogger.log4j2.Controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     String message = "Log4j2 - Spring Boot logger";
+    Logger logger = LogManager.getLogger(LogController.class);
 
     @GetMapping("/")
     public String home() {
+        logger.info("HomeController - OK");
+
         return message;
     }
 }
---
+++ log4j2/src/main/java/com/springbootlogger/log4j2/Controllers/LogController.java
@@ -0,0 +1,24 @@
+package com.springbootlogger.log4j2.Controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+
+    String logMessage = "Logging output via Log4j2..";
+    Logger logger = LogManager.getLogger(LogController.class);
+
+    @GetMapping("/log")
+    public String log() {
+        logger.trace("Log4j2 - A TRACE level message");
+        logger.debug("Log4j2 - A DEBUG level message");
+        logger.info("Log4j2 - A INFO level message");
+        logger.warn("Log4j2 - A WARN level message");
+        logger.error("Log4j2 - A ERROR level message");
+
+        return logMessage;
+    }
+}
