931ff56bfd137b94fb7b79ff9d6ebc4109a6e8b3
Anthony Salemo <ajssalemo@gmail.com>
2021-06-12 16:08:00 -0400

    adding an example with lombok

---
+++ log4j2-lombok/src/main/java/com/springbootlogger/log4j2lombok/Log4j2LombokApplication.java
@@ -0,0 +1,13 @@
+package com.springbootlogger.log4j2lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4j2LombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4j2LombokApplication.class, args);
+	}
+
+}
---
+++ log4j2-lombok/src/test/java/com/springbootlogger/log4j2lombok/Log4j2LombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlogger.log4j2lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4j2LombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
