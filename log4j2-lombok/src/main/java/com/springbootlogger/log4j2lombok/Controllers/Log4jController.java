12f126acc903c06261f4ba5b5f8e4e12019bb03f
Anthony Salemo <ajssalemo@gmail.com>
2021-06-15 16:13:21 -0400

    added and validated that logging works with lombok - log4j/slf4j

---
+++ log4j2-lombok/src/main/java/com/springbootlogger/log4j2lombok/Controllers/Log4jController.java
@@ -0,0 +1,23 @@
+package com.springbootlogger.log4j2lombok.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+public class Log4jController {
+    String message = "Logging output via Log4j2 with Lombok..";
+
+    @GetMapping("/log4j")
+    public String log4j() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
+        return message;
+    }
+}
