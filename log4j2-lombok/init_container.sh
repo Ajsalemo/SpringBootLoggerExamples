572a2e916473c81f79226612735328dae87d0486
Anthony Salemo <ajssalemo@gmail.com>
2022-04-06 23:39:25 -0400

    added this printenv utility to more entrypoint init_container.sh scripts

--- log4j2-lombok/init_container.sh
+++ log4j2-lombok/init_container.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Get env vars in the Dockerfile to show up in the SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
 echo "Starting SSH ..."
 service ssh start
 
